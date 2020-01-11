27c79fa7229d83d5f1d11161f91d2a9e82a8afc9
unknown <mandarsjogalekar@gmail.com>
2020-01-11 17:57:35 +0530

    added BL

---
+++ EmployeeWebApi/EmployeeBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApi
+{
+    public class EmployeeBL
+    {
+        private  readonly SqlConnection _sqlConnection;
+        public EmployeeBL(SqlConnection connection)
+        {
+            _sqlConnection = connection;
+        }
+    }
+}
