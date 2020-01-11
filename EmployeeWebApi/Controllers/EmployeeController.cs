a2b701f214aea7ec62330d3efd964a6f26d97ed4
unknown <mandarsjogalekar@gmail.com>
2020-01-11 17:52:31 +0530

    first commit

---
+++ EmployeeWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeWebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        public EmployeesController()
+        {
+
+        }
+
+        // GET api/employees
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
