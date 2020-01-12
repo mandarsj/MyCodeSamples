912f5c35582a277278bec8bd5dd5aad4c25d5ff5
mandar jogalekar <mandarsjogalekar@gmail.com>
2020-01-12 14:36:42 +0530

    mvc web application

---
+++ EmployeeList/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeList.Models;
+using System.Net.Http;
+
+namespace EmployeeList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            IEnumerable<EmployeeViewModel> employees = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64189/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("employee");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<EmployeeViewModel>>();
+                    readTask.Wait();
+
+                    employees = readTask.Result;
+                }
+                else
+                {
+                    employees = Enumerable.Empty<EmployeeViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(employees);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmployeeList/Models/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeList.Models
+{
+    public class EmployeeViewModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
